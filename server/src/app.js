47bba866815921e384301f4504753c14e8818b70
zainabturkmen <zainabturkmen7@gmail.com>
2024-07-12 09:04:30 +0430

    Adding some express

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
-const path = require("path")
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -11,12 +12,14 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
+
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "public")))
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetsRouter);
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "public", "index.html"))
-})
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
---
+++ server/src/models/launches.model.js
@@ -0,0 +1,19 @@
+const launches = new Map();
+
+const launch = {
+  flightNumber: 100,
+  mission: "Kepler exploration X",
+  rocket: "Explore IS1",
+  launchDate: new Date("December 27, 2030"),
+  destination: "Kepler-442 b",
+  customer: ["ZTM", "NASA"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+// launches.get(100) === launch
+
+module.exports = {
+  launches,
+};
