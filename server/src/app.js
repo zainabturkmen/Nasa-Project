c76a8d3be94bc2174c7756b83d9c02e781af0c8f
zainabturkmen <zainabturkmen7@gmail.com>
2024-07-12 06:20:54 +0430

    Adding some express

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,22 @@
+const path = require("path")
 const express = require("express");
 const cors = require("cors");
 
-const planetsRouter = require("./routes/planets/planets.router")
+const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:3000",
-}))
+  })
+);
 app.use(express.json());
-app.use(planetsRouter)
+app.use(express.static(path.join(__dirname, "..", "public")))
 
-module.exports = app                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+app.use(planetsRouter);
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+})
+
+module.exports = app;
