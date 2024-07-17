3dd47845367739c3550b77c4704b8f127607fef1
zainabturkmen <zainabturkmen7@gmail.com>
2024-07-17 07:50:56 +0430

    Adding some changes

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,14 @@
+describe("Test GET /launches", () => {
+    test("It should response with 200 success", () => {
+        const response = 200;
+        expect(response).toBe(200);
+    })
+});
+
+describe("Test POST /launche", () => {
+    test("IT should response with 200 success", () => {
+
+    })
+    test("It should catch missing required properties", () => { })
+    test("it should catch invalid dates", () => {})
+})
