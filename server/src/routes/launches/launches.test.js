250de127dc8fe1eb62f77d2e4e639a5b136a34bc
zainabturkmen <zainabturkmen7@gmail.com>
2024-07-17 08:26:26 +0430

    Adding some changes

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,14 +1,34 @@
+const request = require("supertest");
+const app = require("../../app");
+
 describe("Test GET /launches", () => {
-    test("It should response with 200 success", () => {
-        const response = 200;
-        expect(response).toBe(200);
-    })
+  test("It should response with 200 success", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-type", /json/)
+      .expect(200);
+  });
 });
 
 describe("Test POST /launche", () => {
-    test("IT should response with 200 success", () => {
+  test("IT should response with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send({
+        mission: "USS Enterprise",
+        rocket: "NCC 1701-D",
+        target: "Kepler-186 f",
+        launchDate: "January 4, 2028",
+      })
+      .expect("Content-type", /json/)
+      .expect(201);
+    expect(response.body).toMatchObject({
+      mission: "USS Enterprise",
+      rocket: "NCC 1701-D",
+      target: "Kepler-186 f",
+    });
+  });
 
-    })
-    test("It should catch missing required properties", () => { })
-    test("it should catch invalid dates", () => {})
-})
+  test("It should catch missing required properties", () => {});
+  test("it should catch invalid dates", () => {});
+});
