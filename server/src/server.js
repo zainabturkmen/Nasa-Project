0fcca8cf7d6b74e6f066d7bc59aeb6fcbe1a2f8d
zainabturkmen <zainabturkmen7@gmail.com>
2024-07-22 11:55:28 +0430

    Adding mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
@@ -6,9 +7,13 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api:xk8Odc62XjTTNlvb@nasacluster.vthvinz.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASACluster";
+
 const server = http.createServer(app);
 
 async function startServer() {
+  
   await loadPlanetsData();
 
   server.listen(PORT, () => {
